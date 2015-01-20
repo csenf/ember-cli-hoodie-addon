24a2abd74b3b4cfc52b9aa5277ec98149f790ee5
csenf <carsten.senf@googlemail.com>
2015-01-20 19:57:30 +0100

    new version 2.6.7, added the hoodie-properties 'type', '_syncedAt'

--- addon/mixins/models/hoodie-model.js
+++ addon/mixins/models/hoodie-model.js
@@ -4,7 +4,9 @@ import DS from 'ember-data';
 export
 default Ember.Mixin.create({
   _rev: DS.attr('string'),
+  _syncedAt: DS.attr('date'),
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date'),
-  createdBy: DS.attr('string')
+  createdBy: DS.attr('string'),
+  type: DS.attr('string')
 });
